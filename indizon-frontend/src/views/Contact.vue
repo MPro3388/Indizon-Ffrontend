0ca9dcc3d53a3e4c1bff62cbe2dfcc835367e799
markoprodanovic <s0543834@htw-berlin.de>
2021-12-13 10:37:18 +0100

    Vue Frontend

---
+++ indizon-frontend/src/views/Contact.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="home">
+    <img alt="Blue Shield logo" src="../assets/BlueShield.png">
+    <HelloWorld msg="Contact Formular"/>
+
+    <form id="contact">
+      <label id="h2" form="contact">Kontaktformular</label>
+      <fieldset>
+        <legend>Name</legend>
+
+        <label for="vorname">
+          Vorname: <input name="vorname" id="vorname" placeholder="Max">
+        </label>
+
+        <label for="nachname">
+          Zuname: <input name="nachname" id="nachname" placeholder="Mustermann">
+        </label>
+
+      </fieldset>
+
+      <fieldset>
+        <legend>Geburtsdatum</legend>
+
+        <label for="birth">
+          Vorname: <input name="birth" id="birth" placeholder="TT.MM.JJJJ">
+        </label>
+
+      </fieldset>
+
+      <fieldset>
+        <legend>Geschlecht</legend>
+
+        <label for="männl">
+          <input type="radio" name="geschlecht" id="männl" value="männl">
+          männlich
+        </label>
+
+        <label for="weibl">
+          <input type="radio" name="geschlecht" id="weibl" value="weibl">
+          weiblich
+        </label>
+
+      </fieldset>
+    </form>
+
+  </div>
+</template>
---
+++ indizon-frontend/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Blue Shield logo" src="../assets/BlueShield.png">
+    <HelloWorld msg="Welcome to Indizon"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
